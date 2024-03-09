8ee3db2be66eb8f06fc899ec22ac9714dc2ffc70
qlovy <qlovy@sysnet.ch>
2024-03-09 18:32:50 +0100

    firstfile

---
+++ README.md
@@ -0,0 +1,20 @@
+# demo-pwa-vue
+Example from here https://vuejs.org/tutorial/#step-8
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <Todo/>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Todo from "@/components/Todo.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Todo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+</style>
