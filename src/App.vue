aeb3377ea75e0d10afa68a050d06eaaadfa7a399
qlovy <qlovy@sysnet.ch>
2024-03-09 20:41:02 +0100

    change what is displayed

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-  <Todo/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Todo />
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Todo from "@/components/Todo.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+//import Todo from "@/components/Todo.vue";
 
 export default {
-  name: 'App',
-  components: {
-    Todo
-  }
-}
+    name: "App",
+    components: {
+        HelloWorld,
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: left;
-  color: #2c3e50;
-  margin-top: 20px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: left;
+    color: #2c3e50;
+    margin-top: 20px;
 }
 </style>
