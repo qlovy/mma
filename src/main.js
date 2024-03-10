19a31b099a64277ed14f529c2370637df027b23a
qlovy <qlovy@sysnet.ch>
2024-03-10 11:58:10 +0100

    Stop try with test.vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import { createApp } from "vue";
-//import App from "./App.vue";
-import test from './test.vue';
+import App from "./App.vue";
+//import test from './test.vue';
 import "./registerServiceWorker";
 
-createApp(test).mount("#test");
-
-//createApp(App).mount("#app");
+//createApp(test).mount("#test");
+createApp(App).mount("#app");
