fe058d234d60440d67e8e2dadeac8aed3070269c
qlovy <qlovy@sysnet.ch>
2024-03-10 11:46:02 +0100

    add func mount

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 //import App from "./App.vue";
 import "./registerServiceWorker";
 import test from './test.vue';
-createApp(test);
+createApp(test).mount();
 
 //createApp(App).mount("#app");
