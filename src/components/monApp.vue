4e0da80d833c9455dfa2e8dc901c34326c7993d8
qlovy <qlovy@sysnet.ch>
2024-03-10 09:43:46 +0100

    update

--- src/components/monApp.vue
+++ src/components/monApp.vue
@@ -1,8 +1,17 @@
-<template>
-    <h1>{{ title }}</h1>
-</template>
-
-<script>
+<script setup>
 import { ref } from "vue";
 const title = ref("MMA");
 </script>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
