1895f688a7e42301ee1901302dec24e987d0fc68
qlovy <qlovy@sysnet.ch>
2024-03-10 11:40:18 +0100

    invers <template> and <script> monApp.vue

--- src/components/monApp.vue
+++ src/components/monApp.vue
@@ -1,10 +1,10 @@
+<template>
+    <h1>MMA</h1>
+    <p>MMA means MMA Musculation App</p>
+</template>
+
 <script>
 export default {
   name: 'monApp',
 };
 </script>
-
-<template>
-    <h1>MMA</h1>
-    <p>MMA means MMA Musculation App</p>
-</template>
