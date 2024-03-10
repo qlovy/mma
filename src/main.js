1afe6c62069b132097208e4759e046c3ee1dd71b
qlovy <qlovy@sysnet.ch>
2024-03-10 12:03:20 +0100

    little update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-//import test from './test.vue';
 import "./registerServiceWorker";
 
-//createApp(test).mount("#test");
 createApp(App).mount("#app");
