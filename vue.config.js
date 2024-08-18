2d4130b0e69aa1a470ba8ce0450e97cd6e72bd58
qlovy <112717381+qlovy@users.noreply.github.com>
2024-08-18 22:13:04 +0200

    everything relative

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/mma/'
-    : '/',
-  transpileDependencies: true,
+  publicPath: './',
+  transpileDependencies: true
 })
