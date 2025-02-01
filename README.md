b143d32caba49ab950986aca48266d43c8c19621
qlovy <qlovy@sysnet.ch>
2025-02-01 13:51:45 +0100

    Change the fill color of the menu icon depend on user input.

--- README.md
+++ README.md
@@ -28,17 +28,17 @@ I choose Vue.js and Bootstrap because Vue make the creation of a PWA more simple
 - [x] Add program "Tirage" and "Jambes"
 - [x] Remove the time after last exercise
 - [x] Add warm-up
-- [ ] make that the sound is like the sound of a timer on iphone , so the music can restart. => try [this](https://stackoverflow.com/questions/59637501/is-there-a-way-of-pausing-background-musicsuch-as-spotify-then-play-a-sound-fi)
-- [x] Change the exercice while the rest time ?
+- [ ] make that the sound is like the sound of a timer on iphone , so the music can restart. => looks complex for a simple PWA
+- [x] Change the exercise while the rest time ?
 - [x] Show the state of series ?
 - [x] upgrade the information (repetitions in seconds, ...) ==> do a function
-- [x] add a button for skip an exercice (like a help button...)
-- [x] add advice for each exercices
+- [x] add a button for skip an exercise (like a help button...)
+- [x] add advice for each exercises
 - [x] add a timer for exercise with time (more than 10 seconds)
-- [ ] change the background color on the top and bottom on iphone, [fix it](https://community.shopify.com/c/shopify-design/color-safari-border/td-p/2862722)
-- [x] create components to simpify the App.vue (home page, exercise page, ...)
-- [ ] make that we can skip a time recuperation / a serie
-- [ ] make the icon menu in color when we are on it => change the fill color, [exapmle](https://icons.getbootstrap.com/icons/gear/)
+- [ ] change the background color on the top and bottom on iphone => looks complex
+- [x] create components to simplify the App.vue (home page, exercise page, ...)
+- [ ] make that we can skip a time recuperation / a series
+- [x] make the icon menu in color when we are on it => change the fill color, [example](https://icons.getbootstrap.com/icons/gear/)
 - [x] use local storage for storing exercises
 - [x] JSON file or text file for add new exercise of configure multiple exercise --> one the local storage is ok
 - [x] add a menu with two categories (one for exercising and the other for settings)
@@ -48,5 +48,5 @@ I choose Vue.js and Bootstrap because Vue make the creation of a PWA more simple
 
 ### Ideas
 
-- Create a tool for create sport program => this is possible by modifying the JSON file and then import it.
+- Create a tool for create sport program => this is possible by modifying the JSON file and then import it. => maybe simple with one config file ?
 
--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,8 @@ const ctx = reactive({
 })
 
 let exerciseArea = ref(true)
+let activityFillColor = "currentColor"
+let gearFillColor = "currentColor"
 
 // Gère l'assignation de la valeur actuelle de référence
 function manageActualUseRef() {
@@ -98,8 +100,6 @@ function resetDefaultProgramme() {
 </script>
 
 <template id="app">
-  <!--Permet aux bandeaux de fond (haut et bas sur Iphone) d'être la même couleur que le body-->
-  <meta content="black-translucent" name="apple-mobile-web-app-status-bar-style">
   <!--Ubuntu Police-->
   <link href="https://fonts.googleapis.com" rel="preconnect">
   <link crossorigin href="https://fonts.gstatic.com" rel="preconnect">
@@ -154,7 +154,12 @@ function resetDefaultProgramme() {
       <tr>
         <td class="align-middle" @click="exerciseArea = true">
           <!--Icon des exercices-->
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+          <svg v-if="exerciseArea" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white"
+               class="bi bi-activity icon-menu" viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+                  d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2"/>
+          </svg>
+          <svg v-else xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
                class="bi bi-activity icon-menu" viewBox="0 0 16 16">
             <path fill-rule="evenodd"
                   d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2"/>
@@ -163,7 +168,14 @@ function resetDefaultProgramme() {
 
         <td class="align-middle" @click="exerciseArea = false">
           <!-- Icon des paramètres-->
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+          <svg v-if="!exerciseArea" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white"
+               class="bi bi-gear icon-menu" viewBox="0 0 16 16">
+            <path
+                d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"/>
+            <path
+                d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z"/>
+          </svg>
+          <svg v-else xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
                class="bi bi-gear icon-menu" viewBox="0 0 16 16">
             <path
                 d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"/>
