f6d7bd175f6c319e38d1e74876d12294335d3610
qlovy <qlovy@sysnet.ch>
2025-02-17 18:53:18 +0100

    Use the programme already stored in the local storage instead of the default one.

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,9 @@ import exerciseDetails from './components/exerciseDetails.vue'
 const defaultProgramme = require("/public/data/programme.json")
 let exercisesBook = defaultProgramme
 
+// Permet d'afficher le programme de l'utilisateur (stocké dans le local storage) à la place du programme par défaut
+updateDB()
+
 // Actualise le livre d'exercices
 function updateDB() {
   // Si l'utilisateur a chargé son programme
