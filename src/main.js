94de85466d4c850e57e50f2e91d24c615739fe08
qlovy <qlovy@sysnet.ch>
2025-05-20 11:24:57 +0200

    Change in function of App.vue content

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import 'bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount("#app");
+createApp(App).mount('#app');
